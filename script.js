758768bfb163e552f59f042b1a85f3b0289c7a97
Yusmary Somaza <yusdev05@gmail.com>
2025-01-31 15:19:16 -0400

    primer commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// MENU BURGER
+const hamburguesa = document.querySelector('#hamburger')
+const enlaces = document.querySelector('#nav__list')
+
+hamburguesa.addEventListener('click', () => {
+    enlaces.classList.toggle('show')
+})
+
+// ANIMACION
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
